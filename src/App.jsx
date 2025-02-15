c8e65d031eb370d595a2e2919ce674dce3fc8b4c
mateuslph <mlptche@gmail.com>
2025-02-15 14:57:10 -0300

    inicia Home page por padrao

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Home from './Home/Home';
 import Projects from './Projects/Projects';
 import RenderFooter from './RenderFooter/RenderFooter';
 import RenderHeader from './RenderHeader/RenderHeader';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sobre from './Sobre/Sobre';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
         <RenderHeader />
         <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />      
           <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<Sobre />} />
